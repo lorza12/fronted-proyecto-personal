17a1f28cb2e69903f7f17b3af0f896d2265aca26
Juan M. Lorza <115027137+lorza12@users.noreply.github.com>
2023-02-22 15:13:45 -0500

    add css (#13)

--- src/components/agend/agend.jsx
+++ src/components/agend/agend.jsx
@@ -1,15 +1,13 @@
 import './agend.css';
-import image from './assest/agendas-08.png';
-import image2 from './assest/agendas fotos-08.png';
 
 const agend = () => (
   <section className="agendContainer">
     <div className="agendContainer__agend">
-      <img src={image} loading="eager" alt="Minibanner" className="fondo" />
+      <img src="https://res.cloudinary.com/df4snwy5t/image/upload/v1677081745/dataFiles/Pagweb_sin-08_cj39aa.png" loading="eager" alt="Minibanner" className="fondo" />
       <button type="button" className="AgendVerMas">
         Ver mas
       </button>
-      <p><img src={image2} loading="eager" alt="Minibanner" className="agenda" /></p>
+      <p><img src="https://res.cloudinary.com/df4snwy5t/image/upload/v1677080215/dataFiles/Pagweb-08_1_fo1at4.png" loading="eager" alt="Minibanner" className="agenda" /></p>
     </div>
   </section>
 );
--- src/components/bannerRedes/bannerRedes.jsx
+++ src/components/bannerRedes/bannerRedes.jsx
@@ -3,26 +3,25 @@ import './bannerRedes.css';
 const bannerRedes = () => (
   <section>
     <section>
-      <hr className="hrBannerRedes" color="F5B2B8" />
       <div className="bannerRedesContainer__title">
-        <h1>INSPÍRATE CON @VELEFANT_</h1>
+        <h1>ENCUENTRA MAS EN @VELEFANT_</h1>
       </div>
     </section>
     <section className="bannerRedesContainer">
       <div className="bannerRedesContainer__img">
-        <h1>img 1</h1>
+        <img src="https://res.cloudinary.com/df4snwy5t/image/upload/v1677083095/dataFiles/Pagweb-14_bv32le.png" alt="img1" />
       </div>
       <div className="bannerRedesContainer__img">
-        <h1>img 2</h1>
+        <img src="https://res.cloudinary.com/df4snwy5t/image/upload/v1677083095/dataFiles/Pagweb-13_xvskg0.png" alt="img2" />
       </div>
       <div className="bannerRedesContainer__img">
-        <h1>img 3</h1>
+        <img src="https://res.cloudinary.com/df4snwy5t/image/upload/v1677083068/dataFiles/Pagweb-11_abwbbl.png" alt="img3" />
       </div>
       <div className="bannerRedesContainer__img">
-        <h1>img 4</h1>
+        <img src="https://res.cloudinary.com/df4snwy5t/image/upload/v1677092015/dataFiles/138ECE4B-E1CE-4679-8C0A-D0262153FB97_hltcff.jpg" alt="img4" />
       </div>
       <div className="bannerRedesContainer__img">
-        <h1>img 5</h1>
+        <img src="https://res.cloudinary.com/df4snwy5t/image/upload/v1677092125/dataFiles/Pagweb-12_1_hugovh.png" alt="img5" />
       </div>
     </section>
   </section>
